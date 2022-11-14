63be85fd51952ce3850fc34fea1f8594ad43e5e3
akoselioren <112801816+akoselioren@users.noreply.github.com>
2022-11-14 08:45:58 +0300

    ikinci hafta projeleri

---
+++ Week_2/methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,11 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dortislem dortIslem = new Dortislem();
+		System.out.println(dortIslem.topla(2, 3));
+		dortIslem.topla(2, 4, 6);
+	}
+
+}
