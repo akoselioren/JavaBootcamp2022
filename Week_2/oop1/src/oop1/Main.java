05773c5b1fd178fba90708579a8b7f72d2f26e71
Abdullah KÖSELİÖREN <112801816+akoselioren@users.noreply.github.com>
2022-11-15 13:03:53 +0300

    Add files via upload

---
+++ Week_2/oop1/src/oop1/Main.java
@@ -0,0 +1,56 @@
+package oop1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String mesaj = "Vade oranı";
+
+		Product product1 = new Product();
+		product1.setName("Delonghi Kahve Makinası");
+		product1.setDiscount(7);
+		product1.setUnitPrice(7500);
+		product1.setUnitsInStock(3);
+		product1.setImageUrl("image1.jpg");
+		Product product2 = new Product();
+		product2.setName("Smag Kahve Makinası");
+		product2.setDiscount(7);
+		product2.setUnitPrice(7500);
+		product2.setUnitsInStock(3);
+		product2.setImageUrl("image2.jpg");
+		Product product3 = new Product();
+		product3.setName("Kitchen Aig Kahve Makinası");
+		product3.setDiscount(7);
+		product3.setUnitPrice(7500);
+		product3.setUnitsInStock(3);
+		product3.setImageUrl("image3.jpg");
+		Product[] products = {product1,product2,product3};
+		
+		System.out.println("<ul>");
+		for (Product product : products) {
+			System.out.println("<li>" + product.getName() + "</li>");
+		}
+		System.out.println("</ul>");
+		
+		IndividualCustomer individualCustemer=new IndividualCustomer();
+		individualCustemer.setId(1);
+		individualCustemer.setPhone("05555555511");
+		individualCustemer.setCustomerNumber("12345");
+		individualCustemer.setFirstName("Abdullah");
+		individualCustemer.setLastName("Köseliören");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
